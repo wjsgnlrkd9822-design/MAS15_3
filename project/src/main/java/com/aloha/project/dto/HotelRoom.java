fec08e6dd9a6b9ecf9713fd3bd44489ae088aae8
wjsgnlrkd9822-design <wjsgnlrkd9822@gmail.com>
2026-01-28 16:22:23 +0900

    메인화면 예약화면 작업중

--- project/src/main/java/com/aloha/project/dto/HotelRoom.java
+++ project/src/main/java/com/aloha/project/dto/HotelRoom.java
@@ -9,10 +9,23 @@ import lombok.ToString;
 @ToString
 public class HotelRoom {
 
-    private Long roomNo;        // 호텔 객실 번호 (PK)
-    private String roomType;    // 객실 호수 (101호, 102호)
+    private Long roomNo;        // 호텔 객실 번호
+    private String roomType;    // 객실 호수
     private int roomPrice;      // 가격
     private String etc;         // 세부 사항
     private String active;      // 예약 여부
     private String img;         // 이미지 파일명
+
+    // ✅ 기본 생성자
+    public HotelRoom() {
+    }
+
+    // ✅ roomNo 제외하고 바로 값 넣을 수 있는 생성자
+    public HotelRoom(String roomType, int roomPrice, String etc, String active, String img) {
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.etc = etc;
+        this.active = active;
+        this.img = img;
+    }
 }
