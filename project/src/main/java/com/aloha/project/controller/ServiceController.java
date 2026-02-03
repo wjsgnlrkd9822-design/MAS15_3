9638500a0264564ec60e01617f9292b25b146891
dbdbstkd735-sudo <dbdbstkd735@gmail.com>
2026-02-03 14:34:55 +0900

    서비스 파일올리기

---
+++ project/src/main/java/com/aloha/project/controller/ServiceController.java
@@ -0,0 +1,28 @@
+package com.aloha.project.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.aloha.project.dto.Trainer;
+import com.aloha.project.service.TrainerService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ServiceController {
+
+    private final TrainerService trainerService;
+    @GetMapping("/service")
+    public String service(Model model) throws Exception {
+        List<Trainer> trainer = trainerService.list();
+        model.addAttribute("trainer", trainer);
+        return "service/service";
+    }
+    
+
+    
+}
